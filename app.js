991b330b0ddf99d35ac7a8c26ff081384d7ba57b
Felipzp123@ <Felipe.ferraz@sptech.school>
2025-05-07 21:31:52 -0300

    mudando app.js

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
+// var ambiente_processo = 'producao';
+var ambiente_processo = 'desenvolvimento';
+
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+
+require("dotenv").config({ path: caminho_env });
+
 var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var PORTA_APP = process.env.APP_PORT || 3000;
+var HOST_APP = process.env.APP_HOST || "localhost";
+
 var app = express();
-var bodyParser = require("body-parser");
 
-var usuarioRoutes = require("./routes/usuarioRoutes");
-var resultadoRoutes = require("./routes/resultadoRoutes");
-var estatisticaRoutes = require("./routes/estatisticaRoutes");
+// Importações ajustadas conforme suas rotas reais
+var usuarioRoutes = require("./src/routes/usuarioRoutes");
+var resultadoRoutes = require("./src/routes/resultadoRoutes");
+var estatisticaRoutes = require("./src/routes/estatisticaRoutes");
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+// Uso das rotas
 app.use("/usuarios", usuarioRoutes);
 app.use("/resultados", resultadoRoutes);
 app.use("/estatisticas", estatisticaRoutes);
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
+// Inicialização do servidor
+app.listen(PORTA_APP, function () {
+    console.log(`
+    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
+    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
+    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
+    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
+    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
+    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
+    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+
+    Servidor rodando em: http://${HOST_APP}:${PORTA_APP}
+    Ambiente: ${process.env.AMBIENTE_PROCESSO}
+    Banco: ${process.env.AMBIENTE_PROCESSO === 'producao' ? 'remoto' : 'local'}
+    `);
 });
