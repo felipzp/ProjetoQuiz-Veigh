22566dd6022bc7f9761d42b1f1847a85f7209ac6
Felipe Ferraz <felipe.ferraz@sptech.school>
2025-05-13 15:40:00 -0300

    Update usuarios.js

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -12,4 +12,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
+router.post("/quiz_inserido", function (req, res) {
+    usuarioController.quiz_inserido(req, res);
+});
+
 module.exports = router;
